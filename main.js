9176beb1387c59eb6a407671c16a4c9ba95b5c0a
Nyandieka <einsteinmokua100@gmail.com>
2024-04-13 17:03:42 +0300

    Likes the messages of other users

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+document.addEventListener('DOMContentLoaded', function() {
+  const EMPTY_HEART = '♡';
+  const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.add('hidden');
 
+  const likeButtons = document.querySelectorAll('.like');
 
+  likeButtons.forEach(likeButton => {
+      likeButton.addEventListener('click', function() {
+          const isLiked = likeButton.classList.contains('activated-heart');
 
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+          mimicServerCall()
+              .then(() => {
+                  if (isLiked) {
+                      likeButton.classList.remove('activated-heart');
+                      likeButton.querySelector('.like-glyph').textContent = EMPTY_HEART;
+                  } else {
+                      likeButton.classList.add('activated-heart');
+                      likeButton.querySelector('.like-glyph').textContent = FULL_HEART;
+                  }
+              })
+              .catch(error => {
+                  const modalMessage = document.getElementById('modal-message');
+                  modalMessage.textContent = error;
+                  
+                  errorModal.classList.remove('hidden');
+                  
+                  setTimeout(() => {
+                      errorModal.classList.add('hidden');
+                  }, 3000);
+              });
+      });
+  });
+});
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
+      setTimeout(function() {
+          let isRandomFailure = Math.random() < .2;
+          if (isRandomFailure) {
+              reject("Random server error. Try again.");
+          } else {
+              resolve("Pretend remote server notified of action!");
+          }
+      }, 300);
   });
 }
